6e0fe45bc9afa912a0d62c807a3781c895fa4f76
Jacob Marshall <Jacobcmarshall1856@gmail.com>
2018-11-10 16:48:38 -0800

    formatted

--- peg_board.py
+++ peg_board.py
@@ -1,14 +1,14 @@
-'''
+"""
     peg_board.py is where the main action happens.
 
     Settings are input from a Unity C# GUI program. Boards are generated into
     ASCII type and translated by the C# program to look nice for a user.
 
     By Spencer Chang, Jacob Marshall
-'''
+"""
 
 import graph_utility as util
 
 if __name__ == "__main__":
-    v = util.Vertex('test');
+    v = util.Vertex("test")
     print("Hello, World!")
